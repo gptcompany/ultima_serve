96fdf2fac55be94a8e021b03a593a53a17b74fce
gawaboga <gawabonga@gmail.com>
2023-11-19 00:12:42 +0000

    update

--- app_serve/app_serve/chain.py
+++ app_serve/app_serve/chain.py
@@ -1,5 +1,13 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GITHUB_ACCESS_TOKEN = os.getenv("GITHUB_ACCESS_TOKEN")
+
 
 _prompt = ChatPromptTemplate.from_messages(
     [
--- app_serve/tests/test_chain.py
+++ app_serve/tests/test_chain.py
@@ -1,5 +1,6 @@
 from app_serve.chain import chain
-
+assert os.environ.get('GITHUB_ACCESS_TOKEN'), 'GITHUB_ACCESS_TOKEN_PERSONAL not found!'
+assert os.environ.get('OPENAI_API_KEY'), 'OPENAI_API_KEY not found!'
 def test_chain():
     print(
           chain.invoke({"test": "1+1=?"})
